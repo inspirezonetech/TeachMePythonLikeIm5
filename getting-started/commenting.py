ad988bfcca6e4f08e56584e92786dfc826e37d04
S10MC2015 <30575036+S10MC2015@users.noreply.github.com>
2020-10-28 23:03:39 +0000

    Update commenting.py
    fixes #16

--- getting-started/commenting.py
+++ getting-started/commenting.py
@@ -1,9 +1,11 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: using comments in you code
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# # is used to add comments to you code. The text in the comment will be ignored by the program itself but can be used to make notes or explain what your code does.
+
+# This is a comment. Yay!
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: 1. create a simple hello world program and comment your name somewhere in the program.
 # ------------------------------------------------------------------------------------
