a8ccaecf02163c5c8b3f2d46463b74fd7e265c60
S10MC2015 <30575036+S10MC2015@users.noreply.github.com>
2020-10-28 23:35:47 +0000

    Update len.py
    #25

--- common-built-in-functions/len.py
+++ common-built-in-functions/len.py
@@ -1,9 +1,19 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: using the len built in function
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# len is used to find the amount of items in an object (ie an array)
+array = ["1","2","3"]
+# the len can be printed
+print("The length of the array is: ", len(array))
+
+# len can also be used to find the amount of characters in a string.
+text = "text"
+# the len can also be stored in a variable
+textlen = len(text)
+print("The amount of characters in the variable text is: ", textlen)
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: 1. Create an array with your own data and get the length of it
+#            2. *assuming you have done the input tutorial* Create a program which asks for someones name and checks the length of it.
 # ------------------------------------------------------------------------------------
