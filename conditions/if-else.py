89bb09d33aa194b00a280136e8bcf4d26f909665
Jorge Madson <filholouco@gmail.com>
2020-10-28 15:41:11 -0300

    Populate if-else
    Teaching if, else, elif

--- conditions/if-else.py
+++ conditions/if-else.py
@@ -1,9 +1,37 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Lets learn how to use if else
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# For example if we want go to play a game that is rated for 12+ year old
+# Lets so we will build a program to check if you can play that game
+
+age = 13
+if age >= 12:
+    print("Yes, you can that game.")
+else:
+    print("Lets find a better game to you.")
+
+# We can also use elif do a chain of conditions
+# like this:
+age = 18
+if age < 18:
+    print("Can't join army")
+elif age > 35:
+    print("Can't join army")
+else:
+    print("Can join army!")
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: Fix the code below to print truly statements
+# Don't change the values, change the ? to right statements
 # ------------------------------------------------------------------------------------
+street_max_speed = 100
+street_min_speed = 100
+car_speed = 200
+
+if ? :
+    print("Lower your speed or you will be penalized")
+elif ? :
+    print("You should get a higher speed")
+else:
+    print("Ok, you are in a good speed to travel")
