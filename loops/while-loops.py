e7693560cfb91da4a5207a371140a831506eaec2
Laksh Gupta <lakshgupta253@gmail.com>
2020-10-29 23:45:56 +0530

    Update while-loops.py

--- loops/while-loops.py
+++ loops/while-loops.py
@@ -1,9 +1,33 @@
 # ------------------------------------------------------------------------------------
 # Tutorial: brief description of tutorial content
 # ------------------------------------------------------------------------------------
+# Here we will look into how to use one of the loops - while loop to generate desired outputs with certain condition
+# Syntax: After while we pass the condition to be checked
+
+t = 0
+while t < 10:
+	t = t + 2
+	print(t)
+
+# Also we can use an |else| stance with the while loop as if the condition don't satisfy and we need a last statement to be executed after the while loop is finished:
+
+t = 0
+while t < 10:
+	t = t + 2
+	print(t)
+else:
+	print("loop finished")
+
+# So above loop will work till the specified condition is satisfied hence will run about 5 times in this case
 
 # Code here explaining concept with comments to guide
-
+# Now as we have learnt about while loops
+# We can try and do a challenge for some fun then
+# Reverse the Number:
+# What we need to do is take n as input and print the reversed n.
+# Example:
+# Input - 2345
+# Output - 5432
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
 # ------------------------------------------------------------------------------------
