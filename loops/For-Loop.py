7f4bcec4cddfcfe50305ae4f95dcead80e431dd2
Dinusha Weerakoon <73241057+Dinushawh@users.noreply.github.com>
2020-10-29 14:53:19 +0530

    Create For-Loop.py

---
+++ loops/For-Loop.py
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------------------------------
+# Tutorial: how to declare and call a for loop
+# ------------------------------------------------------------------------------------
+
+# List of numbers
+numbers = [6, 4, 33, 5, 434, 4, 521]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+	sum = sum+val
+	
+#print the variables on the list
+print("The sum is", sum)
+exit()
+# ------------------------------------------------------------------------------------
+# Challenge: create a for loop that have list and print all the
+# variables on the list
+# ------------------------------------------------------------------------------------
