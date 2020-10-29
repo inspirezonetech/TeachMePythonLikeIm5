e5d3bea745124abb3c946211827f2c0253c44a53
Laksh Gupta <lakshgupta253@gmail.com>
2020-10-29 14:28:33 +0530

    updated range and len

--- common-built-in-functions/len.py
+++ common-built-in-functions/len.py
@@ -2,7 +2,20 @@
 # Tutorial: brief description of tutorial content
 # ------------------------------------------------------------------------------------
 
+
 # Code here explaining concept with comments to guide
+# len function is a in-built func to get the length of desired DS
+
+# l = [1,2,3,4]
+
+# print(len(l)) will return the length of the list or number of elements in the particular list i.e. = 4
+
+# Syntax
+
+len(l)
+
+# In the bracket we can use our DS
+
 
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
--- common-built-in-functions/range.py
+++ common-built-in-functions/range.py
@@ -4,6 +4,20 @@
 
 # Code here explaining concept with comments to guide
 
+# Range func is inbuilt function having arguments passed to it below:
+#		if we use just a single argument range(n) - it takes 0 by default as starting point and follows for n values i.e. ending at n-1
+#  		range(n) = range(0,n) as upper limit is not included , max to max (upper limit - 1) is included in range
+# 		range(l,u,inc) {l = lowerlimit, u = upperlimit, inc = increament like what we need to be the gap between each iteration of numbers
+#		by default if we do not use the third argument it is taken as +1 . inc can be negative as well}
+
+# Syntax:
+
+range(0,n,+1)  #goes from 0 to n-1 with inc of 1 equal to range(n)
+
+range(n,0,-2)  # goes from n to 1 with inc of -2 
+
+# So range deals with both increament as well as decreament hence very useful while calling for loops
+
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
 # ------------------------------------------------------------------------------------
