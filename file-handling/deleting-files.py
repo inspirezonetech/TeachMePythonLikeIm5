0fb87b8004d22904d90fb3adacf047d71a33374b
Dilan David <lord.dabu@gmail.com>
2020-10-29 17:14:34 -0400

    upated delete-files.py to resolve #20

--- file-handling/deleting-files.py
+++ file-handling/deleting-files.py
@@ -1,8 +1,23 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Receives a file name from a user and deletes it
 # ------------------------------------------------------------------------------------
+import os
 
-# Code here explaining concept with comments to guide
+def delete (filename):
+    try:
+        os.remove(filename) #deletes filename
+        print(f"{filename} has been deleted") #prints out success message
+    except OSError:
+        print("No such file in directory!")
+
+def main():
+    #get the filename from the user
+    filename = input("Please enter name (with type) of the file you would like to remove: ")
+    #call delete function from above and pass in the filename
+    delete(filename)
+
+if __name__=="__main__":
+    main()
 
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
