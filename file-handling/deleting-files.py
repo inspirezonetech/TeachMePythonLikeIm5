642862e7cff6889b8c798a66f91c537bf3519e03
Dilan David <lord.dabu@gmail.com>
2020-10-29 22:11:06 -0400

    upated code to fix linting errors

--- file-handling/deleting-files.py
+++ file-handling/deleting-files.py
@@ -1,24 +1,27 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: Receives a file name from a user and deletes it
+#    Tutorial: Receives a file name from a user and deletes it
 # ------------------------------------------------------------------------------------
+
 import os
 
-def delete (filename):
+def delete_file(filename):
     try:
-        os.remove(filename) #deletes filename
-        print(f"{filename} has been deleted") #prints out success message
-    except OSError:
-        print("No such file in directory!")
+        os.remove(filename)  # deletes filename
+        print(f'{filename} has been deleted')  # prints out success message
+    except (OSError, EOFError):
+        print('No such file in directory!')  # prints error message
 
 def main():
-    #get the filename from the user
-    filename = input("Please enter name (with type) of the file you would like to remove: ")
-    #call delete function from above and pass in the filename
-    delete(filename)
+    # get the filename from the user
+    filename = input('Please enter name (with type) of the file you would like to remove: ')
 
-if __name__=="__main__":
+    # call delete function from above and pass in the filename
+    delete_file(filename)
+
+if __name__ == '__main__':
     main()
 
+
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+#   Challenge: Delete a specific file in directory
 # ------------------------------------------------------------------------------------
