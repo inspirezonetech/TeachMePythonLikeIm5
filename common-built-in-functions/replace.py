e58287d04567730bbf5f0508efc3a011302579a6
Simone Faggi <simone.faggi@yahoo.it>
2020-10-30 23:02:04 +0100

    Add replace tutorial and challenge (#51)
    * Add replace tutorial and challenge
    
    * Fix block comment
    
    * Fix block comment
    
    * slight edit to wording in replace.py
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- common-built-in-functions/replace.py
+++ common-built-in-functions/replace.py
@@ -1,9 +1,30 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: The replace method replaces a specified string with another specified string.
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# Example 1.
+# Replace all occurences of "dog" with "cat"
+
+dog_txt = "I always wanted a dog! My dog is awsome :) My dog\'s name is Taco"
+cat_txt = dog_txt.replace("dog", "cat")
+
+print("\nExample 1. - Replace all occurences of \"dog\" with \"cat\"")
+print(f'Old String: {dog_txt}')
+print(f'New String: {cat_txt}')
+
+
+# Example 2.
+# Replace the two first occurences of "dog" with "cat"
+
+dog_txt = "I always wanted a dog! My dog is awsome :) My dog\'s name is Taco"
+cat_txt = dog_txt.replace("dog", "cat", 2)
+
+print("\nExample 2. - Replace first two occurences of \"dog\" with \"cat\"")
+print(f'Old String: {dog_txt}')
+print(f'New String: {cat_txt}\n')
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge:
+# 1. From the string "Teach me Python like Im 5" produce the string "Teach me Java like Im 10".
+# 2. From the string "Im 10 years old" produce the string "How old are you?".
 # ------------------------------------------------------------------------------------
