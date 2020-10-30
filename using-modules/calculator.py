27b4991282a5f4b91f623bd0021c71bcbd01828b
jbolanosc <josue.carit@outlook.com>
2020-10-30 14:39:44 -0600

    Tutorial of importing modules added

--- using-modules/calculator.py
+++ using-modules/calculator.py
@@ -1,7 +1,20 @@
+# ------------------------------------------------------------------------------------
+# Tutorial: Importing Modules in Python
+# ------------------------------------------------------------------------------------
+
+# Create a module with functions inside, in this case
+# A calculator module to sum and multiply
+
 def sum_two_numbers(a, b):
     result = a + b
     return f'result of sum {a} + {b} = {result}'
 
+
 def multiply_two_numbers(a, b):
     result = a * b
     return f'result of multiply {a} X {b} = {result}'
+
+# ------------------------------------------------------------------------------------
+# Challenge: Create a calculator module, with the 4 basics functions
+# sum, multiply, divide, substract
+# ------------------------------------------------------------------------------------
