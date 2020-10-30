3d8f65693607708132eaa6b02dea5f7aaeb06609
Dilan David <lord.dabu@gmail.com>
2020-10-30 15:35:26 -0400

    upated delete-files.py to resolve #20 (#48)
    * upated delete-files.py to resolve #20
    
    * upated code to fix linting errors
    
    * added a catch for EOFError
    
    * added exception to return a default file name

--- file-handling/deleting-files.py
+++ file-handling/deleting-files.py
@@ -1,9 +1,31 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+#    Tutorial: Receives a file name from a user and deletes it
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+import os
+
+def delete_file(filename):
+    try:
+        os.remove(filename)  # deletes filename
+        print(f'{filename} has been deleted')  # prints out success message
+    except OSError:
+        print('No such file in directory!')  # prints error message when file not found
+
+def main():
+    filename = None
+    try:
+        # get the filename from the user
+        filename = input('Please enter name (with type) of the file you would like to remove: ')
+    except EOFError:
+        return filename
+
+    # call delete function from above and pass in the filename
+    delete_file(filename)
+
+if __name__ == '__main__':
+    main()
+
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+#   Challenge: Delete a specific file in directory
 # ------------------------------------------------------------------------------------
