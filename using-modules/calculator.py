fcb847ce4c3d65314a22cda4f3228e7b987c53bf
jbolanosc <josue.carit@outlook.com>
2020-10-30 14:22:37 -0600

    Tutorial of importing modules added

---
+++ using-modules/calculator.py
@@ -0,0 +1,7 @@
+def sum_two_numbers(a, b):
+    result = a + b
+    return f'result of sum {a} + {b} = {result}'
+
+def multiply_two_numbers(a, b):
+    result = a * b
+    return f'result of multiply {a} X {b} = {result}'
--- using-modules/importing-modules.py
+++ using-modules/importing-modules.py
@@ -1,9 +1,30 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Importing Modules in Python
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# Create a module in another file with the functions or data you need
+# Also you can import modules from other libraries with pip
+# In this line you import the module,
+# With the keyword import and the name of your module
+
+import calculator
+
+a = 2
+b = 4
+
+# Here we are calling the function inside the module to get the result
+# Of the sum of number a and b
+result_sum = calculator.sum_two_numbers(a, b)
+
+# Here we are calling the function inside the module to get the result
+# Of the multiplication of number a and b
+result_multiplication = calculator.multiply_two_numbers(a, b)
+
+print(result_sum)
+
+print(result_multiplication)
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: Create a calculator module, with the 4 basics functions 
+# sum, multiply, divide, substract
 # ------------------------------------------------------------------------------------
