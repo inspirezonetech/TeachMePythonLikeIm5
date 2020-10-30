8deb60112af5406956b459afaaa99c0bcd80f8db
Famosi <simone.faggi@yahoo.it>
2020-10-30 09:55:13 +0100

    Fix block comment

--- common-built-in-functions/replace.py
+++ common-built-in-functions/replace.py
@@ -2,7 +2,7 @@
 # Tutorial: The replace method replaces a specified string with another specified string.
 # ------------------------------------------------------------------------------------
 
-# Example 1. 
+# Example 1.
 # Replace all occurences of "dog" with "cat"
 
 dog_txt = "I always wanted a dog! My dog is awsome :) My dog\'s name is Taco"
@@ -24,7 +24,7 @@ print(f'Old String: {dog_txt}')
 print(f'New String: {cat_txt}\n')
 
 # ------------------------------------------------------------------------------------
-# Challenge: 
+# Challenge:
 # 1. From the string "Teach me Python like Im 5" produce the string "Teach me Java like Im 10".
 # 2. From the string "Im 10 years old" produce the string "How old are you?".
 # ------------------------------------------------------------------------------------
