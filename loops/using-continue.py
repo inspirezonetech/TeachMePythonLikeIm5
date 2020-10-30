9e1785df0498f5f92c51338ce835bb81b4811eab
Laksh Gupta <lakshgupta253@gmail.com>
2020-10-30 10:19:21 +0530

    Update using-continue.py

--- loops/using-continue.py
+++ loops/using-continue.py
@@ -1,31 +1,26 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Using continue keyword in python
 # ------------------------------------------------------------------------------------
 
 # It allows us to leave some piece of code when we do not need it or some fixed condition is not met.
 # It is just a keyword which allows us to handle our code in specific conditions.
-# for example if we say that a particular var has achieved a particular val then we don't proceed with the piece of code after the continue keyword
+# for example if we say that a particular var has achieved a particular val then we don't proceed with the piece of code after the continue keyword.
 # Note : We also use it when we have some base cases for our test cases while doing competitive programming.
+# Also note that continue keyword is only used in the case of loops.
 
-# Code here explaining concept with comments to guide
 
-t = 0
-
-for x in range(5):
-    print(t)
-    if t == 3:
+for x in range(5): 
+    if x == 3:
         continue
-    t = t + 1
+    print(x)
+    
 
-# In above example after t approaches val 3 t stops increamenting so hence aur output will be like 0 1 2 3 3 (just concentrate on values not the pattern)
+# In above example as x approaches the value 3 (as in condition) continue statement is trigerred an thus when we look at the output of the code, x=3 is not printed.
 # We can conclude that after the condition achieved our code was not given permission to increase the val of t.
 # Also there are times we don't want to do anything in a particular condition so what we do is just use the continue statement to keep the iteration going on.
 
-
-# As we understood enough of the theory part now we require to implement it and watch how it goes:
-# Challenge:
-# So what we need to do is print only odd numbers upto n where n is user based input using continue statement
+# Basically, the code below the continue keyword is considered only if the above condition does not satisfy else the code will process the request and exclude the lines of code below continue. 
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: Take user input n and print all the odd natural numbers upto n using continue keyword.
 # ------------------------------------------------------------------------------------
