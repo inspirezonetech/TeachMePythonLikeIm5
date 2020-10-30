1212c41b20789db769cb4426f3e3f120c6684af6
jbolanosc <josue.carit@outlook.com>
2020-10-30 00:15:13 -0600

    Added handling-exceptions tutorial

--- exceptions/handling-exceptions.py
+++ exceptions/handling-exceptions.py
@@ -9,18 +9,17 @@
 # It will be handled by the except block,
 # if not the except section will be ignored.
 
-a = 2
 
-while a > 0:
-    try:
-        a = a - 1
-        print("Working!!")
-        if a == 0:
-            # Here a custom exception is raised if a is equal to 0
-            raise ValueError("a cannot be 0")
-    except ValueError:
-        print('Something went wrong')
-        raise
+try:
+    a = 2
+    a = a - 1
+    print("Working!!")
+    if a == 0:
+        # Here a custom exception is raised if a is equal to 0
+        raise ValueError("a cannot be 0")
+except ValueError:
+    print('Something went wrong')
+    raise
 
 # ------------------------------------------------------------------------------------
 # Challenge: Create a try / except block and raise a custom exception when the value received is not a number
