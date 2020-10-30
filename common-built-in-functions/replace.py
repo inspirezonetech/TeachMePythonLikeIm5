1ca2a59fa53ce77814526029c173cffe34eb99f7
Fum <66768334+funbeedev@users.noreply.github.com>
2020-10-30 21:58:21 +0000

    slight edit to wording in replace.py

--- common-built-in-functions/replace.py
+++ common-built-in-functions/replace.py
@@ -19,7 +19,7 @@ print(f'New String: {cat_txt}')
 dog_txt = "I always wanted a dog! My dog is awsome :) My dog\'s name is Taco"
 cat_txt = dog_txt.replace("dog", "cat", 2)
 
-print("\nExample 2. - Replace all occurences of \"dog\" with \"cat\"")
+print("\nExample 2. - Replace first two occurences of \"dog\" with \"cat\"")
 print(f'Old String: {dog_txt}')
 print(f'New String: {cat_txt}\n')
 
