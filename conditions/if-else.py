7f9d36997666679a55838c2e91d6d594f24985c4
Jorge Madson <filholouco@gmail.com>
2020-10-30 16:24:41 -0300

    Make the code runnable

--- conditions/if-else.py
+++ conditions/if-else.py
@@ -23,15 +23,16 @@ else:
 
 # ------------------------------------------------------------------------------------
 # Challenge: Fix the code below to print truly statements
-# Don't change the values, change the ? to right statements
+# Don't change the values, change the replace_this to right statements
 # ------------------------------------------------------------------------------------
 street_max_speed = 100
-street_min_speed = 100
-car_speed = 200
+street_min_speed = 50
+car_speed = 70
 
-if ? :
+replace_me = false
+if replace_me:
     print("Lower your speed or you will be penalized")
-elif ? :
+elif replace_me:
     print("You should get a higher speed")
 else:
     print("Ok, you are in a good speed to travel")
