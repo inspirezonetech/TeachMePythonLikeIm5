b508dfc15042d98e6b252ccb2ac047c27bd17009
Famosi <simone.faggi@yahoo.it>
2020-10-30 09:54:19 +0100

    Fix block comment

--- common-built-in-functions/replace.py
+++ common-built-in-functions/replace.py
@@ -1,19 +1,9 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: 
-# --> The replace method replaces a specified string with another specified string.
-# Syntax: 
-# --> string.replace(oldvalue, newvalue, count)
-# Params:
-# --> oldvalue: Required. The string to replace (str)
-# --> newvalue: Required. The string to replace the old value with (str)
-# --> count: Optional. How many occurrences of the old value you want to replace (int). 
-#                      All if not specified
-# Returns:
-# --> A copy of the string where the occurrence of a substring is replaced with another substring.
+# Tutorial: The replace method replaces a specified string with another specified string.
 # ------------------------------------------------------------------------------------
 
-############### Example 1. ###############
-# Replace all occurences of "dog" with "cat" #
+# Example 1. 
+# Replace all occurences of "dog" with "cat"
 
 dog_txt = "I always wanted a dog! My dog is awsome :) My dog\'s name is Taco"
 cat_txt = dog_txt.replace("dog", "cat")
@@ -23,8 +13,8 @@ print(f'Old String: {dog_txt}')
 print(f'New String: {cat_txt}')
 
 
-############### Example 2. ###############
-# Replace the two first occurences of "dog" with "cat" #
+# Example 2.
+# Replace the two first occurences of "dog" with "cat"
 
 dog_txt = "I always wanted a dog! My dog is awsome :) My dog\'s name is Taco"
 cat_txt = dog_txt.replace("dog", "cat", 2)
