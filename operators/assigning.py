9ab8f53e1d4908bab04720e8c2c51ae0e015837f
Ajordat <alex.jorda@blackdata.systems>
2020-10-31 15:04:41 +0100

    Added further explanations and the final challenge.

--- operators/assigning.py
+++ operators/assigning.py
@@ -23,9 +23,12 @@ var = var + 2
 # then assigned to `var`.
 print(var)  # 3
 
-# In this aspect, there are more assignment operators that pretend to shorten a
-# small operation on the variable and then an assignment to itself. As an
-# example, the following line will do the same as the one above:
+
+# ARITHMETIC AND BITWISE ASSIGNMENTS
+
+# There are more assignment operators that intend to shorten a small operation
+# on the variable and then an assignment to itself. As an example, the
+# following line will do the same as the one above:
 var += 2  # var = var + 2
 # These kind of assignments always add the target variable on the left side of
 # the operation.
@@ -39,6 +42,8 @@ var /= 10   # var = var / 10
 var %= 10   # var = var % 10
 var //= 10  # var = var // 10
 var **= 10  # var = var ** 10
+
+var = 1
 # Bitwise assignments:
 var &= 10   # var = var & 10
 var |= 10   # var = var | 10
@@ -54,14 +59,56 @@ var *= 3 + 2
 # assignment operator has the least priority of all the operators.
 
 
-# HONORABLE MENTIONS
-# - Not strongly typed: var can have different types
-# - Multiple assignments inline: a = b = c
-# - walrus a := exp
-# - Unpacking: a, b = 1, 2
+# TYPING IN ASSIGNMENTS
+
+# In Python, variables are not strongly typed, which means that they can change
+# their type with every assignment.
+var = 1
+print(type(var))  # <class 'int'>
+var = 'string'
+print(type(var))  # <class 'str'>
+
+
+# CHAINED ASSIGNMENT
+
+# On simple assignments `=`, multiple variables can be assigned the same
+# value in the same line, for instance:
+var = var2 = 3
+print(var, var2)  # 3 3
+
+
+# ASSIGNING TUPLES
+
+# When working with tuples it's important to know that its values can be
+# unpacked in different variables with an assignment:
+tup = (1, 2, 3)
+a, b, c = tup
+print(tup[1])  # 2
+print(b)  # 2
+# Of course, this can be used to assign different values to different values
+# on the same line, such as:
+value1, value2 = 10, 2
+print(value1)  # 10
+print(value2)  # 2
+
+
+# WALRUS OPERATOR
+
+# On Python 3.8 the walrus operator `:=` was introduced. Said operator allows
+# to perform an assignment during the evaluation an expression.
+# This allows us to, instead of performing an operation, assigning it to a
+# variable (to later reuse it) and evaluating the result in 2 different lines,
+# it can be done in one.
+# Without the walrus operator:
+result_no_walrus = value1 + value2
+if result_no_walrus > 10:
+    print(result_no_walrus)  # 12
+# With the walrus operator:
+if (result_with_walrus := value1 + value2) > 10:
+    print(result_with_walrus)  # 12
 
 # -----------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per
-# tutorial
+# Challenge: Starting with the tuple (5, 10, 15) get the result of adding each
+# value multiplied by its position in a single variable ONLY using assignment
+# operators. (That's to say: 5 * 1 + 10 * 2 + 15 * 3)
 # -----------------------------------------------------------------------------
-
