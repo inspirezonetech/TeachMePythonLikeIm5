6a247ef67a560ed756d11e50b0f08105f8c68519
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-10-31 15:36:07 +0530

    Removed the function and simplified the code

--- file-handling/reading-files.py
+++ file-handling/reading-files.py
@@ -2,29 +2,19 @@
 # Tutorial: Receives a file name from the user, reads and then displays the contents to the console
 # -------------------------------------------------------------------------------------------------
 
-def read_file(filename):
-    try:
-        fr = open(filename, mode="r")  # Creates a file pointer to the given file name and the mode 'r' opens the file in read mode
-        fcontents = fr.read()  # Reads all the contents of the file into the variable fcontents as a single string
-        print(f"The contents of the file {filename}:")
-        print(fcontents)
-    except FileNotFoundError:
-        print('File not found!')
+# Taking the file we would like to read (Similar to selecting a book you want to read)
+# Make sure you give the full path in case the file is not present in the same location as the program
+filename = input("Enter the file name you would like to read: ")
 
-def main():
-    filename = None
-    try:
-        # get the filename from the user
-        filename = input('Please enter name of the file you would like to read: ')
-    except EOFError:
-        return filename
+# Since we know which file to read let's open it first (We have to open the book first so that we can read)
+fread = open(filename, mode="r")  # Here the r specifies we want to read the file
 
-    # call read_file function from above and pass in the filename
-    read_file(filename)
+# Now since we have the file open we can read it (Once our book is open we can start reading from the book)
+fcontents = fread.read()  # Reads and saves all the contents into the variable fcontents
 
-if __name__ == '__main__':
-    main()
+# As we have read from the file we check out what's present in the file
+print(fcontents)
 
-# ---------------------------------------------------------------------------------------------------------------------
-# Challenge 1: Create a file in the directory with some contents. Run the program and display the contents of that file
-# ---------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------------------------------
+# Challenge: Create a file and add some content into the file. Check whether the output from the program matches the content you have in the file
+# -----------------------------------------------------------------------------------------------------------------------------------------------
