238d38e5da6403e0277faedb73a89b96cd8db688
Laksh Gupta <lakshgupta253@gmail.com>
2020-10-31 13:39:13 +0530

    Update range.py

--- common-built-in-functions/range.py
+++ common-built-in-functions/range.py
@@ -19,9 +19,23 @@ x = range(0, n, 1)  # goes from 0 to n-1 with step size of 1 equal to range(n)
 
 y = range(n, 0, -2)  # goes from n to 1 with step size of -2
 
+# Here I will set up an example for you all to clarify any more doubts about range()
+
+# Example:
+
+t = range(1,11)
+for number in t:
+	if number%2==0:
+		print(number)
+
+
+# The above example gets us all the even numbers within the range [1,10].
+
 # So range deals with both increament as well as decreament hence very useful while calling for loops.
 # Also range() returns a sequence of numbers included in | range(start,stop,step) |
 
 # ------------------------------------------------------------------------------------
-# Challenge: Take user based input n and then compute the sum of first n natural numbers and print the sum.
+# Challenge: Take user based input n and then compute the sum of first n natural numbers(1,2,3,4....,n) and print the sum. Use the range function only and not the direct formula.
+# Example: Input - 5
+#          Output - 15 
 # ------------------------------------------------------------------------------------
