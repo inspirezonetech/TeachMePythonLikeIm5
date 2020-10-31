394063217b2b9a09016e2a1a9638e7c5575114e7
ashutosh007865 <53836584+ashutosh007865@users.noreply.github.com>
2020-10-31 17:43:22 +0530

    Correction2.3
    Trying to resolve issues

--- lists/declaring-lists.py
+++ lists/declaring-lists.py
@@ -5,13 +5,13 @@
 # Code here explaining concept with comments to guide
 # Syntax:
 # empty list
-lst=[]
+lst = []
 # list of integer
-lst=[1 , 2 , 3 , 4 , 5]
+lst = [1, 2, 3, 4, 5]
 # list with various data type
-lst=[1 , "hello", 3.55]
+lst=[1, "hello", 3.55]
 # nested list
-lst=[1 , [8 , 3 , 4 , 5] , "hello"]
+lst=[1, [8, 3, 4, 5], "hello"]
 # retrieval of elements using list index
 print(lst[0])  # this will print first element of the list that is [1]
 # negative index
