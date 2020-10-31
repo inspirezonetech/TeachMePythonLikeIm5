4f698edc1cda78dd53ad47243174cf199db9a5a8
Laksh Gupta <lakshgupta253@gmail.com>
2020-10-31 17:01:57 +0530

    Update for-loops.py

--- loops/for-loops.py
+++ loops/for-loops.py
@@ -10,7 +10,7 @@ anime = ['Luffy','Naruto','Zoro','Midoriya','Lelouch','Levi','Natsu','Sanji','Sh
 # What we have to do is print each of these names in list anime
 
 for name in anime:
-    printf(name)
+    print(name)
 
 # This will help you to access and print each of the names in the list.
 
@@ -21,6 +21,22 @@ length = len(anime)
 for i in range(length):
 	print(anime[i])
 
+# Also there is an |else| stance for each of the loops so here it is for for-loop:
+
+
+for name in anime:
+    print(name)
+else:
+    print('Loop has finished')
+
+# It gives us a message when the loop is finished.
+
+# Practice with the below challenge
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge: You have given a list of numbers and you have to output how many of the numbers are even and how many are odd.
+#            list = [ 3, 21, 4, 56, 34, 69, 45, 63, 99 ]
+
+#            Example : list = [ 2, 4, 5, 8, 34, 21 ]
+#                      Output:- Odd : 2, Even : 4
+#
 # ------------------------------------------------------------------------------------
