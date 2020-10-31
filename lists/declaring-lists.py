8a1f7da28dc5ff116ab9ce5c568bd5b5c45d3296
ashutosh007865 <53836584+ashutosh007865@users.noreply.github.com>
2020-10-31 11:43:40 +0530

    udate the list
    update the file as per instruction given into the file

--- lists/declaring-lists.py
+++ lists/declaring-lists.py
@@ -1,9 +1,16 @@
 # ------------------------------------------------------------------------------------
 # Tutorial: brief description of tutorial content
+  list is a  collection which is ordered and changeable.Allows duplicate members.
 # ------------------------------------------------------------------------------------
 
 # Code here explaining concept with comments to guide
+  Lst=[] #empty list
+  Lst=[1,2,3,4,5] #list containing some elements
+  Lst.append(8) #append function add the passing element into the list in the end. so output = [1,2,3,4,5,8]
+  lst.remove(1) # this function removes the passing element from the list.
+                # there are lots more others which you can findout using ctrl button after list.
 
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+  1. Try to put list into list.
 # ------------------------------------------------------------------------------------
