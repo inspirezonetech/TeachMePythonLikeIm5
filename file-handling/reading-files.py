4cec8b77b0906a0161ec0599e6d052cc7528ca4e
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-10-31 15:44:22 +0530

    Linted the code

--- file-handling/reading-files.py
+++ file-handling/reading-files.py
@@ -5,15 +5,17 @@
 # Taking the file we would like to read (Similar to selecting a book you want to read)
 # Make sure you give the full path in case the file is not present in the same location as the program
 filename = input("Enter the file name you would like to read: ")
+try:
+    # Since we know which file to read let's open it first (We have to open the book first so that we can read)
+    fread = open(filename, mode="r")  # Here the r specifies we want to read the file
+    
+    # Now since we have the file open we can read it (Once our book is open we can start reading from the book)
+    fcontents = fread.read()  # Reads and saves all the contents into the variable fcontents
 
-# Since we know which file to read let's open it first (We have to open the book first so that we can read)
-fread = open(filename, mode="r")  # Here the r specifies we want to read the file
-
-# Now since we have the file open we can read it (Once our book is open we can start reading from the book)
-fcontents = fread.read()  # Reads and saves all the contents into the variable fcontents
-
-# As we have read from the file we check out what's present in the file
-print(fcontents)
+    # As we have read from the file we check out what's present in the file
+    print(fcontents)
+except (EOFError, FileNotFoundError):
+    print("File not found! Please check if the file exists in the same folder or provide the full path.")
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------
 # Challenge: Create a file and add some content into the file. Check whether the output from the program matches the content you have in the file
