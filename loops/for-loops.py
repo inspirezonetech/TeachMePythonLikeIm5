f56648ce07ffec2c18c43d086f1d6a24e502e3e3
Laksh Gupta <lakshgupta253@gmail.com>
2020-10-31 16:16:33 +0530

    Update for-loops.py

--- loops/for-loops.py
+++ loops/for-loops.py
@@ -1,8 +1,25 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: For-loops in Python
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# We use for loops when we have to access elements in a DS or while working within a range of numbers.
+# One of the loops which helps us to iterate through a particular DS.
+
+anime = ['Luffy','Naruto','Zoro','Midoriya','Lelouch','Levi','Natsu','Sanji','Shanks','Kakashi','Kageyama']
+
+# What we have to do is print each of these names in list anime
+
+for name in anime:
+    printf(name)
+
+# This will help you to access and print each of the names in the list.
+
+# Alternate 
+
+length = len(anime)
+
+for i in range(length):
+	print(anime[i])
 
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
