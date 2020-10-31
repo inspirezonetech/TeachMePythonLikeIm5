a97b2e7ff96e7c575d7a565e74eb60d226dab02d
ashutosh007865 <53836584+ashutosh007865@users.noreply.github.com>
2020-10-31 17:14:44 +0530

    Correct the mistakes
    add some correction

--- lists/declaring-lists.py
+++ lists/declaring-lists.py
@@ -19,14 +19,12 @@ lst=[1,[8,3,4,5],"hello"]
 print(lst[0]) #this will print first element of the list that is [1]
 #negative index
 print(lst[-1]) # output: ["hello"]
-#List slicing
-print(lst[1:])
 #there are some in-built function available in the python for the list, like
 lst.append(9) #this will add element in the end of the list
 lst.remove(9) #this will remove the element 9 from the list
 lst.sort()  # this will arrange the element in the decending order
 lst.insert(1,9) #this will add insert the element 9 at index place 1.
-del lst[0;2] # this will delete the element in the range 0-2
+del lst[0:2] # this will delete the element in the range 0-2
 lst.count() #this will return the no of argument passes.
 
 # ------------------------------------------------------------------------------------
