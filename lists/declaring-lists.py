af643bc2a4058e65d8ee7cea4152223c09281b9a
ashutosh007865 <53836584+ashutosh007865@users.noreply.github.com>
2020-11-02 03:57:58 +0530

    Update List-declartion (#65)
    * Update List-declartion
    
    briefly describe the about list and its functions as well, and also add one challenge
    
    * Correct the mistakes
    
    add some correction
    
    * correction.2
    
    whitespace added after #
    
    * correction2.1
    
    more improvements
    
    * Correcetion2.2
    
    Trying to resolve white space issue
    
    * Correction2.3
    
    Trying to resolve issues
    
    * final improvement
    
    whitespace after operator
    
    * Update Declartion-list
    
    minor changes
    
    * Update declaring-lists.py
    
    * Update declaring-lists.py
    
    * Update declaring-lists.py
    
    * Update declaring-lists.py
    
    * Update declaring-lists.py

--- lists/declaring-lists.py
+++ lists/declaring-lists.py
@@ -1,9 +1,29 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# LIST
+# List is a collection which is ordered and changeable.
 # ------------------------------------------------------------------------------------
-
 # Code here explaining concept with comments to guide
-
+# Syntax:
+# empty list
+lst = []
+# list of integer
+lst = [1, 2, 3, 4, 5]
+# list with various data type
+lst = [1, "hello", 3.55]
+# nested list
+lst = [1, [8, 3, 4, 5], "hello"]
+# retrieval of elements using list index
+print(lst[0])  # this will print first element of the list that is [1]
+# negative index
+print(lst[-1])  # output: ["hello"]
+# there are some in-built function available in the python for the list, like
+# lst.sort()  # this will arrange the element in the ascending order.
+lst.append(9)  # this will add element in the end of the list.
+lst.remove(9)  # this will remove the element 9 from the list.
+lst.insert(1, 9)   # this will add insert the element 9 at index place 1.
+del lst[0:2]  # this will delete the element in the range 0-2.
+lst.count(9)   # this will return the no of argument passes.
 # ------------------------------------------------------------------------------------
 # Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# 1. Take input from the user into the empty list using input() function.
 # ------------------------------------------------------------------------------------
