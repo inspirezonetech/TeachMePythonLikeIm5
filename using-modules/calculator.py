1eaf6e36170b6a642bcd02abdb1ef7ef5789df19
Fum <66768334+funbeedev@users.noreply.github.com>
2020-11-02 19:55:29 +0000

    update calculator.py to link to the tutorial

--- using-modules/calculator.py
+++ using-modules/calculator.py
@@ -1,8 +1,9 @@
 # ------------------------------------------------------------------------------------
 # Tutorial: Importing Modules in Python
+# This file is the calculator module created for the "creating-modules.py" tutorial
 # ------------------------------------------------------------------------------------
 
-# Create a module with functions inside, in this case
+# Creating a module with functions inside, in this case
 # A calculator module to sum and multiply
 
 def sum_two_numbers(a, b):
@@ -15,6 +16,12 @@ def multiply_two_numbers(a, b):
     return f'result of multiply {a} X {b} = {result}'
 
 # ------------------------------------------------------------------------------------
-# Challenge: Create a calculator module, with the 4 basics functions
-# sum, multiply, divide, substract
+# Challenge: (Part of the challenge with "creating-modules.py")
+# Add two more functions to this module: divide_two_numbers, subtract_two_numbers.
+# Then call the functions from "creating-modules.py"
 # ------------------------------------------------------------------------------------
+
+# Here define function called divide_two_numbers
+
+
+# Here define function called subtract_two_numbers
