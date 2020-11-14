524e7d88fa136c92605cb275ee55763fe9563576
Fum <66768334+funbeedev@users.noreply.github.com>
2020-11-14 18:27:26 +0000

    small edits to formatting

--- variables/booleans.py
+++ variables/booleans.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Understanding Boolean variables
 # ------------------------------------------------------------------------------------
 # Boolean variables are defined by the True and False keywords
 # eg: a = True
@@ -10,30 +10,37 @@
 # It returns False if the parameter or value passed is False with some exceptions such as
 # If False value is passed, None is passed, an empty sequence is passed such as (), [], ”, etc, or if Zero is passed in any numeric type such as 0, 0.0, etc.
 
-# Code here explaining concept with comments to guide
 def Boolean():
+
     # Returns True as x is True
     x = True
     print(bool(x))
+
     # Returns False as x is False
     x = False
     print(bool(x))
+
     # Returns False as x is not equal to y
     x = 15
     y = 18
     print(bool(x == y))
+
     # Returns False as x is None
     x = None
     print(bool(x))
+
     # Returns False as x is an empty sequence
     x = ()
     print(bool(x))
+
     # Returns False as x is an emty mapping
     x = []
     print(bool(x))
+
     # Returns False as x is 0
     x = 0.00
     print(bool(x))
+
     # Returns True as x is a non empty string
     x = 'This is Boolean Tutorial'
     print(bool(x))
@@ -49,4 +56,4 @@ def Challenge(arg1, arg2, arg3):
 Challenge([], 125, True)
 
 # --------------------------------------CHALLENGE----------------------------------------------
-# Challenge: call the Challege function and pass the values '0.00', '{1, 341, 56}' and 'False' to the function
+# Challenge: call the Challenge function and pass the values '0.00', '{1, 341, 56}' and 'False' to the function
