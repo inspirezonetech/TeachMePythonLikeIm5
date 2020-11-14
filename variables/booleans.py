4ebc71b2c4644f7f64c3f233f55a4202f11a770d
Dave3130 <52663557+Dave3130@users.noreply.github.com>
2020-11-15 00:01:35 +0530

    added booleans.py (#84)
    * added booleans.py
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- variables/booleans.py
+++ variables/booleans.py
@@ -1,9 +1,59 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Tutorial: Understanding Boolean variables
 # ------------------------------------------------------------------------------------
+# Boolean variables are defined by the True and False keywords
+# eg: a = True
+# We can check type of variables by printing : type(var_name)
+# If it is of type boolean it will return : <class 'bool'>
+# bool() method can be used to convert value to boolean value
+# It returns True if the parameter or value passed is True
+# It returns False if the parameter or value passed is False with some exceptions such as
+# If False value is passed, None is passed, an empty sequence is passed such as (), [], ”, etc, or if Zero is passed in any numeric type such as 0, 0.0, etc.
 
-# Code here explaining concept with comments to guide
+def Boolean():
 
-# ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
-# ------------------------------------------------------------------------------------
+    # Returns True as x is True
+    x = True
+    print(bool(x))
+
+    # Returns False as x is False
+    x = False
+    print(bool(x))
+
+    # Returns False as x is not equal to y
+    x = 15
+    y = 18
+    print(bool(x == y))
+
+    # Returns False as x is None
+    x = None
+    print(bool(x))
+
+    # Returns False as x is an empty sequence
+    x = ()
+    print(bool(x))
+
+    # Returns False as x is an emty mapping
+    x = []
+    print(bool(x))
+
+    # Returns False as x is 0
+    x = 0.00
+    print(bool(x))
+
+    # Returns True as x is a non empty string
+    x = 'This is Boolean Tutorial'
+    print(bool(x))
+
+Boolean()
+
+def Challenge(arg1, arg2, arg3):
+    print("Challenge:")
+    print(bool(arg1))
+    print(bool(arg2))
+    print(bool(arg3))
+
+Challenge([], 125, True)
+
+# --------------------------------------CHALLENGE----------------------------------------------
+# Challenge: call the Challenge function and pass the values '0.00', '{1, 341, 56}' and 'False' to the function
