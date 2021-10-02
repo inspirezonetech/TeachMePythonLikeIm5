61ba6bd2adbfd7287e9ff886d5aca4c1818b4190
Renato771 <renato771@tutanota.com>
2021-10-02 20:04:03 -0300

    Improved functions and booleans

--- functions/using-functions.py
+++ functions/using-functions.py
@@ -21,3 +21,32 @@ another_function('hello', 'universe')
 # Challenge: create a function that takes two integers as arguments and prints the sum.
 # call the function and pass the values '1' and '2' to the function
 # ------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+# ---------------------------------------------------
+# More on functions
+# ---------------------------------------------------
+
+# Functions can return something with the 'return' keyword.
+def biggestNumber(number1, number2):
+    maxnumber = max(number1, number2) # The max keyword returns the highest value of all those passed
+
+    return maxnumber
+
+# When calling the function, you can use the value returned
+oldestPerson = biggestNumber(15, 18)
+print("Oldest person:", oldestPerson)
+
+print( biggestNumber(100*0, 1*2) )
+
+# -----------------------------------------------------------------------------------
+# Challenge: modify the previous function you create so it returns the value instead.
+# create another piece of code that uses the value returned from that function
+# ------------------------------------------------------------------------------------
+
