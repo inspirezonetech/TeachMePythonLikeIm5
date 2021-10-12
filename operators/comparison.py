08f21dc66596eb3da5f81c49d9358c9cd57ec161
Abhijeet Gupta <53190754+abhijeetgupto@users.noreply.github.com>
2021-10-12 18:06:31 +0530

    Added tutorial for Comparison Operators !  (#92)
    * added the tutorial for comparision operators
    
    * Added tutorial for comparision operators
    
    * Added tutorial for comparison operators.
    
    * fixed Run flint with flake8 error
    
    * fixed Run flint with flake8 error
    
    * fixed Run flint with flake8 error
    
    * Update comparison.py
    
    Co-authored-by: abhijeetgupto <>
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- operators/comparison.py
+++ operators/comparison.py
@@ -1,9 +1,44 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: brief description of tutorial content
+# Comparision Operators in Python
 # ------------------------------------------------------------------------------------
 
-# Code here explaining concept with comments to guide
+# So basically there are 6 Comparison Operators in Python , let us learn them one by one .
+# Note : One more thing to note is that whenever you compare two "things" , the return value of this Comparision will be either True or False
+
+# 1. "=="(Equal) -> we use this operator to see if two variables are equal.
+a = 5
+b = 5
+print(a == b)  # Here since a and b are equal we will get True in output.
+
+# 2. "!="(Not Equal) -> we use this operator to see if two variables are equal or not , if they are not equal it will return True else False.
+a = 5
+b = 5
+print(a != b)  # Here since a and b are equal we will get False in  output.
+
+# 3. ">"(Greater than ) -> we use this operator to see if one variable is greater than other.
+a = 9
+b = 5
+print(a > b)  # Here since a is greater than b ,  we will get True in output.
+
+# 4. ">="(Greater than or equal to ) -> we use this operator to see if two variables are equal or one is grater than the other.
+a = 5
+b = 5
+print(a >= b)  # Here since a and b are equal,  we will get True in output.
+
+# 5. "<"(Less than ) -> we use this operator to see if one variable is less than other.
+a = 9
+b = 5
+print(a < b)  # Here since a is greater than b ,  we will get False in  output.
+
+# 6. "<="(Less than or equal to ) -> we use this operator to see if two variables are equal or one is less than the other.
+a = 3
+b = 5
+print(a <= b)  # Here since a is less than b ,  we will get True in output.
 
 # ------------------------------------------------------------------------------------
-# Challenge: list challenges to be completed here. minimum of one challenge per tutorial
+# Challenge for you :
 # ------------------------------------------------------------------------------------
+
+# Given a array of numbers . Traverse the array and find out if the number at ith position is less than , greater than or equal to 10.
+nums = [1, 3, 10, -7, 8]
+# write your code here
