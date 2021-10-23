f299e426e78148018f9e5c5db1d4d27b46a5c8c0
Fum <66768334+funbeedev@users.noreply.github.com>
2021-10-23 14:32:11 +0100

    format to comply with flake8 linting

--- functions/recursive_func.py
+++ functions/recursive_func.py
@@ -1,26 +1,23 @@
 # ------------------------------------------------------------------------------------
-# Tutorial: how to make a recursive function
+# Tutorial: How to make a recursive function
+#  A recursive function is a function that calls itself
 # ------------------------------------------------------------------------------------
 
 # find the factorial of a user entered number
 # the 'def' keyword is used to declare a function. the structure of a function is:
 mul = 1
 def fact(num):
-    if num ==1:
+    if num == 1:
         return num
     else:
-        return num * fact(num-1)
+        return num * fact(num - 1)
 
 num = int(input('Enter a number to get the factorial of it: '))
-if num == 0 :
+if num == 0:
     print("Factorial of 0: 1")
 else:
-    print("Factorial of ", num , ":", fact(num))
-
-
+    print("Factorial of ", num, ":", fact(num))
 
 # -----------------------------------------------------------------------------------
 # Challenge: create a recursive function that makes a countdown from a user entered number till zero
 # ------------------------------------------------------------------------------------
-
-
