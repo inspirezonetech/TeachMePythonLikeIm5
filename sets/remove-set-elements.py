935c05a8944f0fdfa092c619fc3be088a0523721
RAKSHA V G <vgraksha26@gmail.com>
2021-10-30 14:23:19 +0530

    more on remove sets

--- sets/remove-set-elements.py
+++ sets/remove-set-elements.py
@@ -27,6 +27,28 @@ print(my_set)
 my_set.discard(2)
 print(my_set)
 
+# Similarly, we can remove and return an item using the pop() method.
+# Since set is an unordered data type, there is no way of determining which item will be popped. It is completely arbitrary.
+# We can also remove all the items from a set using the clear() method.
+
+# initialize my_set
+# Output: set of unique elements
+my_set = set("HelloWorld")
+print(my_set)
+
+# pop an element
+# Output: random element
+print(my_set.pop())
+
+# pop another element
+my_set.pop()
+print(my_set)
+
+# clear my_set
+# Output: set()
+my_set.clear()
+print(my_set)
+
 # ------------------------------------------------------------------------------------
 # Challenge: Remove an element which is not present in my_set and print the set. Observe the output. Check which type of error it is and why it occurs.
 # ------------------------------------------------------------------------------------
