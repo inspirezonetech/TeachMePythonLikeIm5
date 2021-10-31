2a765e9caf9d50496f081fa993826e67f24a1abd
Guy Swowden <guyden1997@gmail.com>
2021-11-01 07:09:46 +1100

    Update if-else.py (#117)
    * Update if-else.py
    
    Update  if-else.py to fix grammatical errors and simplify some of the wording, as well as adding a new example demonstrating using the equal and not equals operator.
    
    * syntax changes
    
    * fix minor typos
    
    Co-authored-by: Fum <66768334+funbeedev@users.noreply.github.com>

--- conditions/if-else.py
+++ conditions/if-else.py
@@ -2,18 +2,31 @@
 # Tutorial: Lets learn how to use if else
 # ------------------------------------------------------------------------------------
 
-# For example if we want go to play a game that is rated for 12+ year old
-# Lets so we will build a program to check if you can play that game
+# For this example we are programming a game that uses lives,
+# if a user runs out of lives the program will display a game
+# over message.
+
+lives = 0
+
+if lives == 0:         # the variable lives is equal to 0.
+    print("Game Over.")
+elif lives != 0:       # the variable lives is not equal to 0.
+    print("Ha Ha Ha, Staying Alive.")
+
+# For example if we want play a game that is rated 12 years and up
+# Lets write a program to check if you can play that game.
 
 age = 13
+
 if age >= 12:
     print("Yes, you can play that game.")
 else:
-    print("Lets find a better game to you.")
+    print("Lets find a better game for you.")
+
 
 # We can also use elif do a chain of conditions
 # like this:
-age = 18
+
 if age < 18:
     print("Can't join army")
 elif age > 35:
@@ -22,17 +35,18 @@ else:
     print("Can join army!")
 
 # ------------------------------------------------------------------------------------
-# Challenge: Fix the code below to print truly statements
-# Don't change the values, change the replace_me to right statements
+# Challenge: Using your knowledge of operators, write two conditions where it
+# says "replace-me", to check if your car is going below speed limit and above
+# the minimum speed for the street.
 # ------------------------------------------------------------------------------------
+
 street_max_speed = 100
 street_min_speed = 50
 car_speed = 70
 
-replace_me = False
-if replace_me:
+if "replace_me":
     print("Lower your speed or you will be penalized")
-elif replace_me:
+elif "replace_me":
     print("You should get a higher speed")
 else:
     print("Ok, you are in a good speed to travel")
