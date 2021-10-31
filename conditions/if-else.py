3383d2c6abde6d1764e7db719f4de9a924cc9402
Kurt Brummert <iguessso99@yahoo.com>
2021-10-30 21:44:15 -0400

    Update if-else.py

--- conditions/if-else.py
+++ conditions/if-else.py
@@ -5,25 +5,41 @@
 # For example if we want go to play a game that is rated for 12+ year old
 # Lets so we will build a program to check if you can play that game
 
+# Example using >= operator
 age = 13
 if age >= 12:
     print("Yes, you can play that game.")
 else:
-    print("Lets find a better game to you.")
+    print("Let's find a better game for you.")
+    
+# Example using == operator    
+age = 17
+if age == 16:
+    print("You can have your Sweet 16 party this year.")
+else:
+    print("You are either too old or too young for a Sweet 16 party.")
 
+# Example using != operator    
+age = 16
+if age != 16:
+    print("You are either too old or too young for a Sweet 16 party.")
+else:
+    print("You can have your Sweet 16 party this year.")
+    
 # We can also use elif do a chain of conditions
 # like this:
 age = 18
 if age < 18:
-    print("Can't join army")
+    print("Can't join army.")
 elif age > 35:
-    print("Can't join army")
+    print("Can't join army.")
 else:
     print("Can join army!")
-
+    
+    
 # ------------------------------------------------------------------------------------
-# Challenge: Fix the code below to print truly statements
-# Don't change the values, change the replace_me to right statements
+# Challenge: Fix the code below to print true statements.
+# Don't change the values. Change the replace_me to the correct statements.
 # ------------------------------------------------------------------------------------
 street_max_speed = 100
 street_min_speed = 50
