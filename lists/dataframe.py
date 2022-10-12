8090bb344c943d09ac524704e6e8280e088ab46f
Ifeoma Igwe <110051825+ifeomaaaa@users.noreply.github.com>
2022-10-12 15:09:57 +0100

    updated list to dataframe using flake8

--- lists/dataframe.py
+++ lists/dataframe.py
@@ -6,13 +6,13 @@
 # Remember a list is created using square brackets []. So list of lists is different lists all wraped in a square bracket i.e. [['Sophie'], ['Jade']]
 
 # Firstly, import pandas as pd
-import pandas as pd   
+import pandas as pd
 # Creating a list of lists with two strings and an integer each
 list = [['Purple Hibiscus', 'Chimamanda', 2001], ['Things Fall Apart', 'Chinua', 1980]]
 
 # Storing the transformation from list to dataframe in a variable
 # df means dataframe but you can call your variable anything
-df = pd.DataFrame(list, columns =['BookTitle', 'AuthorFirstName', 'YearPublished']) # Naming the columns corresponsing to the lists
+df = pd.DataFrame(list, columns=['BookTitle', 'AuthorFirstName', 'YearPublished'])  # Naming the columns corresponsing to the lists
 df
 
 # There are different ways to create a dataframe from lists. This was just one of them!
@@ -21,5 +21,5 @@ df
 # Challenge: get the right answer !
 #  1. Make your own list of lists
 #  2. Store it in another variable that is not called df
-#  3. Create a dataframe from it 
+#  3. Create a dataframe from it
 # ------------------------------------------------------------------------------------
