ae1f9f78c6d86b51b6aaffda530038c9c810cd60
Your Name <you@example.com>
2022-10-13 20:25:57 +0530

    resolved all the errors for flake8

--- Tkinter/tut1.py
+++ Tkinter/tut1.py
@@ -8,26 +8,26 @@
 
 # Firstly, we would start with how to create a simple tkinter window
 
-    #importing modules first
-from tkinter import * #by this you can call all functions of that particular module at single time
-root=Tk()     # for versions earlier than python3 , t is in small case in Tk i.e. tk()
-root.mainloop()  #mainloop is our ending line for any tkinter window, without it the application will not run
+# importing modules first
+from tkinter import Tk
+# from tkinter import Frame
+
+root = Tk()     # for versions earlier than python3 , t is in small case in Tk i.e. tk()
+root.mainloop()  # mainloop is our ending line for any tkinter window, without it the application will not run
 
 # NOW Time to do some basic config with our window. Like you can give Title and, dimensions to it,
-#  can make it resizable upto some limit or even fix the size 
+# can make it resizable upto some limit or even fix the size
 
-main=Tk()
+main = Tk()
 main.title("resizing window")
 # .geometry("widthxheight")
-main.geometry("500x400")   #the window will be having this size when program starts
-main.minsize(300, 200)    #the lower you can resize it
-main.maxsize(600, 500)    #the maximum size upto which you can stretch it
+main.geometry("500x400")   # the window will be having this size when program starts
+main.minsize(300, 200)    # the lower you can resize it
+main.maxsize(600, 500)    # the maximum size upto which you can stretch it
 main.mainloop()
 
-
-
 # you can even fix the position at which the window should pop at start
-# for this first get some knowledge of dimensions of your desktop. 
+# for this first get some knowledge of dimensions of your desktop
 # the uppermost left position acts like origin where x=y=0
 # like we move downwards , y increases and when we move towards right , x increases
 # the last point or point having maximum values is at bottom right of yor desktop
