d812147bfa99d5edb9f6d49bf3de7d33186bb924
Ifeoma Igwe <110051825+ifeomaaaa@users.noreply.github.com>
2022-10-16 22:58:31 +0100

    Update dataframe.py

--- lists/dataframe.py
+++ lists/dataframe.py
@@ -5,17 +5,18 @@
 # Here I'll show you how to create a datframe with three columns using a list of lists.
 # Remember a list is created using square brackets []. So list of lists is different lists all wraped in a square bracket i.e. [['Sophie'], ['Jade']]
 # PS: anywhere you see a step, remove the hashtag before the code and remove everything in the bracket (). This does not include step 2!
-# e.g. in your practice, copy only "import pandas as pd" not "# import pandas as pd (step 1)"
+# e.g. in your practice, copy only "import pandas as pd" not "# import pandas as pd (step 2)"
 
-# Firstly, import pandas as pd
-# import pandas as pd (step 1)
+# Firstly, pip install pandas then import pandas as pd
+# pip install pandas (step 1)
+# import pandas as pd (step 2)
 # Creating a list of lists with two strings and an integer each
-list = [['Purple Hibiscus', 'Chimamanda', 2001], ['Things Fall Apart', 'Chinua', 1980]]  # Step 2
+list = [['Purple Hibiscus', 'Chimamanda', 2001], ['Things Fall Apart', 'Chinua', 1980]]  # Step 3
 
 # Storing the transformation from list to dataframe in a variable
 # df means dataframe but you can call your variable anything
-# df = pd.DataFrame(list, columns=['BookTitle', 'AuthorFirstName', 'YearPublished']) (Step 3 which is naming the columns corresponsing to the lists)
-# df (step 4)
+# df = pd.DataFrame(list, columns=['BookTitle', 'AuthorFirstName', 'YearPublished']) (Step 4)
+# df (step 5)
 
 # There are different ways to create a dataframe from lists. This was just one of them!
 
